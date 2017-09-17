cf26b0b16883e9b5abdb98f401d40739d8f82514
Janet <janetowen07@gmail.com>
2017-09-17 20:49:58 +0100

    renders the list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Todo from './component/Todo';
+
 class App extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    todos: this.props.todos
+  };
+}
+
   render() {
     return (
       <div className="App">
         <h1>To Do List</h1>
-        
+        <ul>
+          {this.state.todos.map((todo, i) => {
+            return (
+              <Todo key={i}
+                task={todo.task}
+                complete={todo.complete}
+                />
+            );
+          })}
+        </ul>
       </div>
     );
   }
